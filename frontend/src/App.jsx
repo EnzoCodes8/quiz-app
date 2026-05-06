28390ec116fbb91732fc83488556e8cefa03baef
EnzoCodes8 <laurenceopena.maninang@gmail.com>
2026-05-07 03:50:04 +0800

    UI Optimizations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
     console.log("Fetched questions:", data);
 
     setQuestions(data);
-    setSelectedSubject(subject);
+    setSelectedSubject(subject.label);
     setCurrentIndex(0);
     setSelectedAnswer(null);
     setScore(0);
